468b0bb6c3b2b963239bede1dfae2bdaf037e552
Roniel Contreras <ronielcontreras70@gmail.com>
2024-09-07 12:55:04 -0500

    Add sparring history view and table components

---
+++ src/components/SparringHistoryTable.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-data-table-virtual
+    :items="[...Array(10)].map(() => sparring)"
+    :headers="headers"
+  >
+
+  </v-data-table-virtual>
+</template>
+
+<script setup lang="ts">
+  const headers = [
+    { title: 'Esgrimistas', align: 'center', value: 'name' },
+    { title: 'Duración', align: 'center', value: 'duration' },
+    { title: 'Fecha', align: 'center', value: 'date' },
+    { title: 'Dobles', align: 'center', value: 'doubles' },
+  ]
+
+  const sparring = { name: 'Juan / Carlos', duration: '5m 30s', date: '10/10/2024', doubles: '2' }
+</script>
---
+++ src/pages/sparring-history.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container fluid>
+    <v-card
+      title="Historial de Sparrings"
+      flat
+    >
+      <template v-slot:text>
+        <v-text-field
+        label="Buscar"
+        prepend-inner-icon="mdi-magnify"
+        variant="outlined"
+        hide-details
+        single-line
+      />
+      </template>
+
+      <SparringHistoryTable/>
+
+    </v-card>
+  </v-container>
+</template>
