af16f54795035665974c9a142b7e9b69bc62d751
Roniel Contreras <ronielcontreras70@gmail.com>
2024-09-08 22:23:07 -0500

    Add [id] fight page

---
+++ src/pages/fights/[id].vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container fluid>
+    <v-card
+      title="Detalles del combate"
+      flat
+    >
+      <template v-slot:text>
+        <v-timeline side="end">
+          <FightScoreTimelineItem v-for="score in scores" :score="score"/>
+        </v-timeline>
+      </template>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+import Score, { ExchangeType } from '@/models/Score';
+
+
+let scores: Score[] = [
+  {
+    time: "00:14",
+    attackType: "Estocada",  
+    region: "Cabeza",
+    exchange: ExchangeType.POINT,
+  },
+
+  {
+    time: "00:28",
+    attackType: "Estocada",  
+    region: "Cabeza",
+    exchange: ExchangeType.DOUBLE
+  },
+
+  {
+    time: "00:52",
+    attackType: "Estocada",  
+    region: "Cabeza",
+    exchange: ExchangeType.POINT
+  }
+]
+
+</script>
