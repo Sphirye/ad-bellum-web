1ba2597783baac8510298572549072dbdfe5524a
Roniel Contreras <ronielcontreras70@gmail.com>
2024-09-09 22:52:01 -0500

    Add divider and reduce padding in Card component

--- src/components/ui/Card.vue
+++ src/components/ui/Card.vue
@@ -1,12 +1,14 @@
 <template>
   <v-card variant="flat" border="opacity-25 md">
       <v-card-title>
-        <v-sheet color="#BD4545" class="py-3 px-5 my-2">
+        <v-sheet color="grey-darken-1" class="py-3 px-3 my-2">
           <v-row no-gutters align="center">
             <slot name="title"/>
           </v-row>
         </v-sheet>
       </v-card-title>
+
+      <v-divider opacity="25" class="mx-9"/>
 
       <v-card-text>
         <slot name="text"/>
