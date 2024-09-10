0efc45c7c3c9d060a9dde8993c7d1349ab30454d
Roniel Contreras <ronielcontreras70@gmail.com>
2024-09-09 22:35:21 -0500

    Add default Card component

---
+++ src/components/ui/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card variant="flat" border="opacity-25 md">
+      <v-card-title>
+        <v-sheet color="#BD4545" class="py-3 px-5 my-2">
+          <v-row no-gutters align="center">
+            <slot name="title"/>
+          </v-row>
+        </v-sheet>
+      </v-card-title>
+
+      <v-card-text>
+        <slot name="text"/>
+      </v-card-text>
+  </v-card>
+</template>
