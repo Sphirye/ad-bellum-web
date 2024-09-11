c28fa9b9d3029681cb1f5e5263abce76c40cf6ed
Roniel Contreras <ronielcontreras70@gmail.com>
2024-09-10 22:51:42 -0500

    Refactor MatchFencerOverview.vue styles

--- src/components/match/MatchFencerOverview.vue
+++ src/components/match/MatchFencerOverview.vue
@@ -4,37 +4,77 @@
   >
     <template v-slot:title>
       <div class="d-flex justify-space-between">
-        <div class="d-flex">
-          <div class="d-flex flex-column">
-            <span class="text-h6 font-weight-bold">
-              {{  fencer == 1 ? 'Rojo' : 'Negro'  }}
-            </span>
 
-            <span class="text-subtitle-2 text-medium-emphasis">
-              {{ fencer == 1 ? match.fencer_1 : match.fencer_2 }}
-            </span>
-          </div>
+        <div class="d-flex flex-column">
+          <span class="text-h6 font-weight-bold">
+            {{  fencer == 1 ? 'Rojo' : 'Negro'  }}
+          </span>
+
+          <span
+            class="text-subtitle-2 text-medium-emphasis d-inline-block text-truncate"
+            :style="`max-width: ${$vuetify.display.width > 420 ? '200px' : '70px'};`"
+          >
+            {{ fencer == 1 ? match.fencer_1 : match.fencer_2 }}
+          </span>
         </div>
 
-        4 pts
+        <div class="d-flex flex-column">
+          <span>
+            <v-icon icon="mdi-check-bold" size="20"/>
+            4
+          </span>
+
+          <span>
+            <v-icon icon="mdi-alert" size="20"/>
+            3
+          </span>
+        </div>
+        
       </div>
     </template>
 
-    <v-divider color="grey-lighten-5" class="mx-3 mb-2"/>
 
-    <v-card-item class="my-0 py-0" title="Puntuaciones">
-      <v-card-subtitle>
-        Estocadas: 1
-      </v-card-subtitle>
+    <v-row no-gutters justify="center" v-if="false">
+      <v-col cols="12" sm="6">
+        <v-card-item class="my-0 py-0 text-center text-sm-start">
+          <template v-slot:title>
+            <v-icon icon="mdi-check-bold" size="20" class="mb-1"/>
+            Puntuaciones
+          </template>
+    
+          <v-card-subtitle>
+            Estocadas: 1
+          </v-card-subtitle>
+    
+          <v-card-subtitle>
+            Cortes: 1
+          </v-card-subtitle>
+    
+          <v-card-subtitle>
+            Rebanadas: 1
+          </v-card-subtitle>
+        </v-card-item>
+      </v-col>
 
-      <v-card-subtitle>
-        Cortes: 1
-      </v-card-subtitle>
+      <v-col cols="12" sm="6">
+        <v-card-item class="my-0 pt-0 text-center text-sm-start">
 
-      <v-card-subtitle>
-        Rebanadas: 1
-      </v-card-subtitle>
-    </v-card-item>
+          <template v-slot:title>
+            <v-icon icon="mdi-alert" size="20" class="mb-1"/>
+            Penalizaciones
+          </template>
+    
+          <v-card-subtitle>
+            -Atacó  despues del alto.
+          </v-card-subtitle>
+    
+          <v-card-subtitle>
+            -Le dijo marica al réreri.
+          </v-card-subtitle>
+        </v-card-item>
+      </v-col>
+    </v-row>
+
   </v-card>
 </template>
 
