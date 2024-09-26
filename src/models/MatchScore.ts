baee0b478a44e13df74b1045ad7595df09f36aba
Roniel Contreras <ronielcontreras70@gmail.com>
2024-09-26 09:36:28 -0500

    Refactor and add Json properties to MatchScore.ts

--- src/models/MatchScore.ts
+++ src/models/MatchScore.ts
@@ -1,14 +1,36 @@
-export enum ExchangeType {
+import { JsonConverter, JsonCustomConvert, JsonProperty } from "json2typescript"
+
+export enum Verdict {
   DOUBLE = "DOUBLE",
   POINT = "POINT",
   NO_EXCHANGE = "NO_EXCHANGE",
   NO_QUALITY = "NO_QUALITY"
 }
 
+@JsonConverter
+class VerdictConverter implements JsonCustomConvert<Verdict> {
+    deserialize(data: string): Verdict {
+        return (<any>Verdict)[data]
+    }
+    serialize(data: Verdict): any {
+        return data.toString()
+    }
+}
+
 export default class MatchScore {
-  time?: string = undefined
+  @JsonProperty("id", Number)
+  id?: number = undefined
+
+  @JsonProperty("timestamp", String)
+  timestamp?: string = undefined
+
   region?: string = undefined
+
   warnings?: [string] = undefined
-  attackType?: string = undefined
-  exchange?: ExchangeType = undefined
+
+  @JsonProperty("timestamp", String)
+  pointType?: string = undefined
+
+  @JsonProperty("verdict", VerdictConverter)
+  verdict?: Verdict = undefined
 }
