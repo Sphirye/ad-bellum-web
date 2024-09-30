e7741f8309b9908200ea1e0fb4de5dbb14b059f8
Roniel Contreras <ronielcontreras70@gmail.com>
2024-09-30 17:20:20 -0500

    Refactor ConstantTool.ts

--- src/services/tools/ConstantTool.ts
+++ src/services/tools/ConstantTool.ts
@@ -1,10 +1,5 @@
-import { config } from "dotenv"
-config()
-
 export default class ConstantTool {
 
-    static readonly BASE_URL = process.env.VUE_APP_BASE_URL
-    
-    static readonly DEVELOPMENT_DIALOG = process.env.VUE_APP_DEVELOPMENT_DIALOG
+    static readonly BASE_URL = import.meta.env.VITE_APP_BASE_URL
 
 }
