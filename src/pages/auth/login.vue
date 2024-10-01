3fabe8466092a584bfa87ecca2cb288b00d31fd3
Roniel Contreras <ronielcontreras70@gmail.com>
2024-09-30 23:32:52 -0500

    Add /auth/login page

---
+++ src/pages/auth/login.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-container>
+      <v-row dense justify="center" align="center">
+          <v-col cols="12" xs="8" sm="5" md="6">
+            <Card :loading="loading">
+              <template v-slot:title>
+                Iniciar sesión
+              </template>
+
+              <template v-slot:text>
+                <v-row dense>
+                  <v-col cols=12>
+                    <v-text-field
+                      v-model="email"
+                      label="Email"
+                      rounded="0"
+                      variant="outlined"
+                      density="comfortable"
+                      hide-details
+                    />
+                  </v-col>
+
+                  <v-col cols=12>
+                    <v-text-field
+                      v-model="password"
+                      label="Contraseña"
+                      rounded="0"
+                      variant="outlined"
+                      density="comfortable"
+                      hide-details
+                    />
+                  </v-col>
+                </v-row>
+              </template>
+
+              <template v-slot:actions>
+                <v-spacer/>
+                 
+                <v-btn
+                  variant="flat"
+                  color="success"
+                  @click="authenticate()"
+                >
+                  Acceder
+                </v-btn>
+              </template>
+            </Card>
+          </v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Handler from '@/handlers/Handler';
+import { SingleItem } from '@/handlers/interfaces/ContentUI';
+import LoginResponse from '@/models/responses/LoginResponse';
+import AuthService from '@/services/AuthService';
+
+export default {
+  data() {
+    return {
+      loading: false,
+      email: "",
+      password: "",
+    }
+  },
+
+  methods: {
+    async authenticate() {
+      let token: SingleItem<LoginResponse> = { item: {} }
+
+      await Handler.getItem(this, token, () =>
+        AuthService.login(this, this.email, this.password)
+      )
+
+    }
+  }
+}
+</script>
