8d41c7550f37a4df3846eeb9b2245583cb49d7e3
Roniel Contreras <ronielcontreras70@gmail.com>
2024-10-01 00:22:01 -0500

    Add token field in pinia app store

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -2,7 +2,19 @@
 import { defineStore } from 'pinia'
 
 export const useAppStore = defineStore('app', {
-  state: () => ({
-    //
-  }),
+  state: () => {
+    return {
+      token: undefined as String | undefined
+    }
+  },
+
+  actions: {
+    setToken(token: string) {
+      this.token = token
+    },
+
+    clearAuthentication() {
+      this.token = undefined
+    },
+  }
 })
