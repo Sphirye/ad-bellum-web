89e1d4ca1ccf2ca34f224ec76acef08012d747bd
Roniel Contreras <ronielcontreras70@gmail.com>
2024-10-04 22:50:26 -0500

    Refactor fencers page

--- src/pages/fencers/index.vue
+++ src/pages/fencers/index.vue
@@ -1,32 +1,59 @@
 <template>
-  <v-container>
-    <v-row dense justify="center" align="center">
-      <v-col cols="12">
+  <v-container fluid>
+    <Card>
+      <template v-slot:title>
+        <v-btn variant="text" icon @click="$router.back()" class="mr-2" density="compact">
+          <v-icon icon="mdi-arrow-left"/>
+        </v-btn>
+        Esgrimistas
+
+        <v-spacer/>
+
+        <v-btn color="primary" variant="flat">
+          Añadir
+        </v-btn>
+      </template>
+
+      <template v-slot:text>
         <v-data-table-virtual
           :headers="headers"
+          :items="fencers.items"
         />
-      </v-col>
-    </v-row>
+      </template>
+    </Card>
   </v-container>
 </template>
 
 <script lang="ts">
+import Handler from '@/handlers/Handler';
+import { MultipleItem } from '@/handlers/interfaces/ContentUI';
+import Fencer from '@/models/Fencer';
 import FencerService from '@/services/FencerService';
 
 export default defineComponent({
   data() {
     return {
+      router: useRouter(),
       headers: [
         { title: 'Nombre', align: 'center', value: 'name' },
-        { title: 'Fecha de creación', align: 'center', value: 'duration' },
-        { title: 'Creado por', align: 'center', value: 'date' },
+        { title: 'Fecha de creación', align: 'center', value: 'createdDate' },
+        { title: 'Creado por', align: 'center', value: 'createdBy' },
       ] as any,
-      fencers: [],
+      fencers: { items: [], totalItems: 0 } as MultipleItem<Fencer>,
+      loading: false
     }
   },
 
   created() {
-    FencerService.getFencers(this)
+    this.getFencers()
+  },
+
+  methods: {
+    async getFencers() {
+      await Handler.getItems(this, this.fencers, () =>
+        FencerService.getFencers(this)
+      )
+    }
   }
 })
 </script>
