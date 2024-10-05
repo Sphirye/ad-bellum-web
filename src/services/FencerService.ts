d659a696d33eafb1db2fd7b39e56286454ba6e89
Roniel Contreras <ronielcontreras70@gmail.com>
2024-10-04 22:23:40 -0500

    Add FencerService.ts

---
+++ src/services/FencerService.ts
@@ -0,0 +1,37 @@
+import Fencer from "@/models/Fencer"
+import ConstantTool from "./tools/ConstantTool"
+import JsonTool from "./tools/JsonTool"
+import Response from "@/models/responses/Response"
+import { useAppStore } from "@/stores/app"
+import { ComponentPublicInstance } from "vue"
+
+export default class FencerService {
+    static async getFencers(component: ComponentPublicInstance): Promise<Response<Fencer[]>> {
+        try {
+            const response = await component.axios.get(ConstantTool.BASE_URL + '/fencer', {
+                headers: {
+                    Authorization: useAppStore().token?.toString()
+                }
+            })
+            const fencers = JsonTool.jsonConvert.deserializeArray(response.data, Fencer)
+            return Promise.resolve({ result: fencers })
+        } catch(e) {
+            return Promise.reject(e)
+        }
+    }
+
+    static async postFencer(component: ComponentPublicInstance, payload: Fencer) {
+        try {
+            const response = await component.axios.post(ConstantTool.BASE_URL + '/fencer', {
+                headers: {
+                    Authorization: useAppStore().token?.toString()
+                },
+                payload
+            })
+            const fencer = JsonTool.jsonConvert.deserializeObject(response.data, Fencer)
+            return Promise.resolve({ result: fencer })
+        } catch(e) {
+            return Promise.reject(e)
+        }
+    }
+}
