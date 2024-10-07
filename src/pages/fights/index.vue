8f2422c3cd5aefe8e4ab8a88b97138c4e024bf3f
Roniel Contreras <ronielcontreras70@gmail.com>
2024-10-06 21:19:30 -0500

    Add redirect button to create fight view

--- src/pages/fights/index.vue
+++ src/pages/fights/index.vue
@@ -1,25 +1,45 @@
 <template>
-    <v-container fluid>
-      <v-row justify="center">
-        <v-col cols="11">
-          <Card>
-            <template v-slot:title>
-              Historial de Combates
-            </template>
-  
-            <template v-slot:text>
-              <v-text-field
-                label="Buscar"
-                prepend-inner-icon="mdi-magnify"
-                variant="outlined"
-                hide-details
-                single-line
-              />
+  <v-container fluid>
+    <v-row justify="center">
+      <v-col cols="11">
+        <Card>
+          <template v-slot:title>
+            Historial de Combates
 
-              <MatchesHistoryTable/>
-            </template>
-          </Card> 
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
+            <v-spacer/>
+
+            <v-btn
+              color="primary"
+              variant="flat"
+              @click="router.push('/fights/create')"
+            >
+              Crear
+            </v-btn>
+          </template>
+
+          <template v-slot:text>
+            <v-text-field
+              label="Buscar"
+              prepend-inner-icon="mdi-magnify"
+              variant="outlined"
+              hide-details
+              single-line
+            />
+
+            <MatchesHistoryTable/>
+          </template>
+        </Card> 
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+  export default defineComponent({
+    data() {
+      return {
+        router: useRouter(),
+      }
+    }
+  })
+</script>
