0fb245dda50897f7fd9b076b13eb3d2ad9e6994a
Roniel Contreras <ronielcontreras70@gmail.com>
2024-10-07 09:52:32 -0500

    Add getMatches request in MatchService.ts

--- src/services/MatchService.ts
+++ src/services/MatchService.ts
@@ -6,6 +6,20 @@ import Response from "@/models/responses/Response";
 
 export default class MatchService {
 
+    static async getMatches(component: ComponentPublicInstance): Promise<Response<Match[]>> {
+        try {
+            const response = await component.axios.get(ConstantTool.BASE_URL + '/match', {
+                headers: {
+                    Authorization: useAppStore().session.token,
+                }
+            })
+            const matches = JsonTool.jsonConvert.deserializeArray(response.data, Match)
+            return Promise.resolve({ result: matches })
+        } catch(e) {
+            return Promise.reject(e)
+        }
+    }
+
     static async postMatch(component: ComponentPublicInstance, payload: Match): Promise<Response<Match>> {
         try {
             const response = await component.axios.post(ConstantTool.BASE_URL + '/match', payload,{
