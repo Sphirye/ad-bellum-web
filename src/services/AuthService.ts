4d568f64700ad658925d4c90400f497a9dd6ef51
Roniel Contreras <ronielcontreras70@gmail.com>
2024-10-06 20:52:13 -0500

    Refactor Session.ts usages

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,7 +1,7 @@
 import { ComponentPublicInstance } from "vue";
 import ConstantTool from "./tools/ConstantTool";
 import JsonTool from "./tools/JsonTool";
-import LoginResponse from "@/models/responses/LoginResponse";
+import Session from "@/models/Session";
 
 export default class LoginService {
     static async login(component: ComponentPublicInstance, email: string, password: string) {
@@ -9,7 +9,7 @@ export default class LoginService {
             const response = await component.axios.post(ConstantTool.BASE_URL + "/auth/login",{
                 email, password
             })
-            const loginResponse = JsonTool.jsonConvert.deserializeObject(response.data, LoginResponse)
+            const loginResponse = JsonTool.jsonConvert.deserializeObject(response.data, Session)
 
             return Promise.resolve({result: loginResponse})
         } catch (e) {
