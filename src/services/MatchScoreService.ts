8c93d953fc066e721eba9c2b17ecbadda7e56deb
Roniel Contreras <ronielcontreras70@gmail.com>
2024-10-08 22:53:03 -0500

    Add MatchScoreService.ts

---
+++ src/services/MatchScoreService.ts
@@ -0,0 +1,38 @@
+import { useAppStore } from "@/stores/app"
+import ConstantTool from "./tools/ConstantTool"
+import MatchScore from "@/models/MatchScore"
+import JsonTool from "./tools/JsonTool"
+import Response from "@/models/responses/Response"
+
+export default class MatchScoreService {
+
+    static async getScores(component: ComponentPublicInstance, example: MatchScore): Promise<Response<MatchScore[]>> {
+        try {
+            const response = await component.axios.get(
+                ConstantTool.BASE_URL + '/match-score', 
+                {
+                    headers: { Authorization: useAppStore().session.token },
+                    data: example,
+                }
+            )
+            const scores = JsonTool.jsonConvert.deserializeArray(response.data, MatchScore)
+            return Promise.resolve({ result: scores })
+        } catch(e) {
+            return Promise.reject(e)
+        }
+    }
+
+    async postScore(component: ComponentPublicInstance, score: MatchScore) {
+        try {
+            const response = await component.axios.get(ConstantTool.BASE_URL + '/match-score', {
+                    headers: { Authorization: useAppStore().session.token }
+                }
+            )
+            const score = JsonTool.jsonConvert.deserializeObject(response.data, MatchScore)
+            return Promise.resolve({ result: score })
+        } catch(e) {
+            return Promise.reject(e)
+        }
+    }
+
+}
