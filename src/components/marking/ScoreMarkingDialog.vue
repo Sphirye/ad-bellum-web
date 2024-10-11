58f32c24f7039e085767ced5230a1cba485a494a
Roniel Contreras <ronielcontreras70@gmail.com>
2024-10-10 20:46:41 -0500

    Add v-form compoent and use rules to conditionally validate score type field

--- src/components/marking/ScoreMarkingDialog.vue
+++ src/components/marking/ScoreMarkingDialog.vue
@@ -1,39 +1,67 @@
 <template>
-  <v-card
-		border="white md"
-		class="mx-auto border-md"
-    color="#242424"
-  >
-    <template v-slot:title>
-      <div class="d-flex align-center justify-space-between text-medium-emphasis">
-        Marcar Punto
-        <v-btn
-          icon="mdi-close"
-          variant="text"
-          density="compact"
-         />
-      </div>
-    </template>
+  <v-form ref="form">
 
-		<v-divider color="white" thickness="2" class="mx-2"/>
+    <v-card
+      border="white md"
+      class="mx-auto border-md"
+      color="#242424"
+    >
+      <template v-slot:title>
+        <div class="d-flex align-center justify-space-between text-medium-emphasis">
+          Marcar Punto
+          <v-btn
+            icon="mdi-close"
+            variant="text"
+            density="compact"
+          />
+        </div>
+      </template>
 
-		<v-card-text>
+      <v-divider color="white" thickness="2" class="mx-2"/>
 
-      <v-item-group selected-class="border-primary" class="mb-4">
-        <v-row dense>
-          <v-item v-slot="{ selectedClass, toggle }">
+      <v-card-text>
+
+        <v-item-group selected-class="border-primary" class="mb-4">
+          <v-row dense>
+            <v-item v-slot="{ selectedClass, toggle }">
+              <v-col cols="6">
+                <v-card
+                  :class="[selectedClass]"
+                  color="rgba(189,69,69,255)"
+                  @click="toggle"
+                >
+                  <template v-slot:title>
+                    <div class="d-flex justify-space-between">
+                      <div class="d-flex flex-column">
+                        <span class="text-h6 font-weight-bold">Rojo</span>
+                        <span class="text-subtitle-2 text-medium-emphasis">Carlos</span>
+                      </div>
+                      <v-checkbox hide-details/>
+                    </div>
+                  </template>
+                  <v-divider color="white" thickness="2" class="mx-1"/>
+      
+                  <template v-slot:actions>
+                    <v-btn
+                      variant="outlined"
+                      append-icon="mdi-alert-outline"
+                      block
+                    >
+                      Penalizar
+                    </v-btn>
+                  </template>
+                </v-card>
+              </v-col>
+            </v-item>
+    
             <v-col cols="6">
-              <v-card
-                :class="[selectedClass]"
-                color="rgba(189,69,69,255)"
-                @click="toggle"
-              >
+              <v-card color="#494949" @click="">
                 <template v-slot:title>
                   <div class="d-flex justify-space-between">
                     <div class="d-flex flex-column">
-                      <span class="text-h6 font-weight-bold">Rojo</span>
-                      <span class="text-subtitle-2 text-medium-emphasis">Carlos</span>
-                    </div>
+                      <span class="text-h6 font-weight-bold">Negro</span>
+                      <span class="text-subtitle-2 text-medium-emphasis">Juan</span>
+                    </div>  
                     <v-checkbox hide-details/>
                   </div>
                 </template>
@@ -50,121 +78,97 @@
                 </template>
               </v-card>
             </v-col>
-          </v-item>
-  
-          <v-col cols="6">
-            <v-card color="#494949" @click="">
-              <template v-slot:title>
-                <div class="d-flex justify-space-between">
-                  <div class="d-flex flex-column">
-                    <span class="text-h6 font-weight-bold">Negro</span>
-                    <span class="text-subtitle-2 text-medium-emphasis">Juan</span>
-                  </div>  
-                  <v-checkbox hide-details/>
-                </div>
-              </template>
-              <v-divider color="white" thickness="2" class="mx-1"/>
-  
-              <template v-slot:actions>
-                <v-btn
-                  variant="outlined"
-                  append-icon="mdi-alert-outline"
-                  block
-                >
-                  Penalizar
-                </v-btn>
-              </template>
-            </v-card>
+          </v-row>
+        </v-item-group>
+
+
+        <v-row>
+          <v-col cols="12">
+
+            <v-select
+              v-model="score.type"
+              :rules="[isCleanScore() ? rules.required : true]"
+              :disabled="!isCleanScore()"
+              :items="pointTypes"
+              item-title="name"
+              item-value="value"
+              rounded="0"
+              label="Tipo de punto"
+              variant="outlined"
+              density="comfortable"
+              hide-details
+              dense
+              class="mb-2"
+            />
+
+            <v-select
+              rounded="0"
+              label="Flanco"
+              variant="outlined"
+              density="comfortable"
+              :items="flanks"
+              hide-details
+              dense
+            />
+
+            <div class="my-2">
+              <v-switch
+                v-model="score.afterblow"
+                :disabled="!isCleanScore()"
+                @change="onAfterblowChanged()"
+                density="compact"
+                color="primary"
+                label="Afterblow"
+                hide-details
+                inset
+              />
+    
+              <v-switch
+                v-model="score.control"
+                :disabled="!isCleanScore()"
+                @change="onControlChanged()"
+                density="compact"
+                color="primary"
+                label="Control"
+                hide-details
+                inset
+              />
+            </div>
+
+            <v-divider color="white" thickness="2" class="mx-2"/>
+
+            <div class="d-flex justify-space-between wrap">
+              <v-switch
+                v-model="score.verdict"
+                :value="verdict.DOUBLE"
+                :false-value="verdict.POINT"
+                color="primary"
+                label="Doble"
+                hide-details
+                inset
+              />
+    
+              <v-switch
+                v-model="score.verdict"
+                :true-value="verdict.NO_EXCHANGE"
+                :false-value="verdict.POINT"
+                color="primary"
+                label="No exchange"
+                hide-details
+                inset
+              />
+            </div>
           </v-col>
         </v-row>
-      </v-item-group>
+      </v-card-text>
 
-
-      <v-row>
-        <v-col cols="12">
-
-          <v-select
-            v-model="score.type"
-            :items="pointTypes"
-            item-title="name"
-            item-value="value"
-            rounded="0"
-            label="Tipo de punto"
-            variant="outlined"
-            density="comfortable"
-            hide-details
-            dense
-            class="mb-2"
-          />
-
-          <v-select
-            rounded="0"
-            label="Flanco"
-            variant="outlined"
-            density="comfortable"
-            :items="flanks"
-            hide-details
-            dense
-          />
-
-          <div class="my-2">
-            <v-switch
-              v-model="score.afterblow"
-              :disabled="!isCleanScore()"
-              @change="onAfterblowChanged()"
-              density="compact"
-              color="primary"
-              label="Afterblow"
-              hide-details
-              inset
-            />
-  
-            <v-switch
-              v-model="score.control"
-              :disabled="!isCleanScore()"
-              @change="onControlChanged()"
-              density="compact"
-              color="primary"
-              label="Control"
-              hide-details
-              inset
-            />
-          </div>
-
-          <v-divider color="white" thickness="2" class="mx-2"/>
-
-          <div class="d-flex justify-space-between wrap">
-            <v-switch
-              v-model="score.verdict"
-              :value="verdict.DOUBLE"
-              :false-value="verdict.POINT"
-              color="primary"
-              label="Doble"
-              hide-details
-              inset
-            />
-  
-            <v-switch
-              v-model="score.verdict"
-              :true-value="verdict.NO_EXCHANGE"
-              :false-value="verdict.POINT"
-              color="primary"
-              label="No exchange"
-              hide-details
-              inset
-            />
-          </div>
-        </v-col>
-      </v-row>
-		</v-card-text>
-
-    <v-card-actions>
-      <v-btn variant="outlined" @click="postScore()">
-        Marcar
-      </v-btn>
-    </v-card-actions>
-
-  </v-card>
+      <v-card-actions>
+        <v-btn variant="outlined" @click="postScore()">
+          Marcar
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-form>
 </template>
 
 <script lang="ts">
@@ -172,6 +176,7 @@ import Handler from '@/handlers/Handler';
 import Match from '@/models/Match';
 import MatchScore, { PointType, Verdict } from '@/models/MatchScore';
 import MatchScoreService from '@/services/MatchScoreService';
+import Rules from '@/services/tools/Rules';
 
 export default defineComponent({
   props: {
@@ -193,6 +198,7 @@ export default defineComponent({
       ],
       loading: false,
       verdict: Verdict,
+      rules: Rules,
     }
   },
 
@@ -202,11 +208,14 @@ export default defineComponent({
 
   methods: {
     async postScore() {
-      let score = { item: new MatchScore() }
-
-      await Handler.getItem(this, score, () =>
-        MatchScoreService.postScore(this, this.score)
-      )
+      if (await this._isFormValid()) {
+        let score = { item: new MatchScore() }
+  
+        await Handler.getItem(this, score, () =>
+          MatchScoreService.postScore(this, this.score)
+        )
+      }
+      
     },
 
     resetScore() {
@@ -229,7 +238,12 @@ export default defineComponent({
       if (this.score.control) {
         this.score.afterblow = false
       }
-    }
+    },
+
+    async _isFormValid() {
+        const { valid } = await this.$refs.form.validate()
+        return valid as boolean
+      },
   },
 })
 </script>
