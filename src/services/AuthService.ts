80018bde88676abd8f83cb6bd7da8549a544a166
Roniel Contreras <ronielcontreras70@gmail.com>
2024-10-16 13:36:53 -0500

    Add checkAuth request in AuthService.ts

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -2,9 +2,11 @@ import { ComponentPublicInstance } from "vue";
 import ConstantTool from "./tools/ConstantTool";
 import JsonTool from "./tools/JsonTool";
 import Session from "@/models/Session";
+import Response from "@/models/responses/Response";
 
 export default class LoginService {
     static async login(component: ComponentPublicInstance, email: string, password: string) {
+        component.axios.defaults.withCredentials = true
         try {
             const response = await component.axios.post(ConstantTool.BASE_URL + "/auth/login",{
                 email, password
@@ -12,6 +14,17 @@ export default class LoginService {
             const loginResponse = JsonTool.jsonConvert.deserializeObject(response.data, Session)
 
             return Promise.resolve({result: loginResponse})
+        } catch (e) {
+            return Promise.reject(e)
+        }
+    }
+
+    static async checkAuth(component: ComponentPublicInstance): Promise<Response<Session>> {
+        try {
+            const response = await component.axios.get(ConstantTool.BASE_URL + "/auth/check")
+            const session = JsonTool.jsonConvert.deserializeObject(response.data, Session)
+
+            return Promise.resolve({result: session})
         } catch (e) {
             return Promise.reject(e)
         }
