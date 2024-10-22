e2654eb775c4155efd6dd91c63f5c42b1059f3a4
Roniel Contreras <ronielcontreras70@gmail.com>
2024-10-21 23:31:28 -0500

    Add dialog component

---
+++ src/components/ui/Dialog.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-dialog>
+    <Card>
+      <template v-slot:title>
+        {{ dialogStore.title }}
+      </template>
+
+      <template v-slot:text>
+        {{ dialogStore.body }}
+      </template>
+
+      <template v-slot:actions>
+        <v-spacer/>
+        <v-btn color="secondary" tile>Cancelar</v-btn>
+				<v-btn color="primary" tile>Aceptar</v-btn>
+      </template>
+    </Card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { useDialogStore } from '@/stores/dialog';
+
+export default defineComponent({
+  data() {
+    return {
+      dialogStore: useDialogStore(),
+    }
+  }
+})
+</script>
