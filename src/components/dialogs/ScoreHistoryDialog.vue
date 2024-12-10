3c01fa1f351e0d90687785bc93110d1505133b04
Roniel Contreras <ronielcontreras70@gmail.com>
2024-12-10 11:27:06 -0500

    Add ScoreHistoryDialog.vue

---
+++ src/components/dialogs/ScoreHistoryDialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-dialog
+    v-model="dialog"
+    fullscreen
+  >
+    <v-card>
+      <template v-slot:title>
+        Historial de puntos
+      </template>
+
+      <template v-slot:text>
+      </template>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+
+export default defineComponent({
+  data() {
+    return {
+      dialog: false,
+      loading: false,
+    }
+  }
+})
+</script>
