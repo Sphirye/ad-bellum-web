800613ab8f0eccb90486fb0879497995e92c61f6
Roniel Contreras <ronielcontreras70@gmail.com>
2024-12-16 17:18:25 -0500

    Add UserService.ts

---
+++ src/services/UserService.ts
@@ -0,0 +1,56 @@
+import User from "@/models/User";
+import ConstantTool from "./tools/ConstantTool";
+import { useAppStore } from "@/stores/app";
+import JsonTool from "./tools/JsonTool";
+
+export default class UserService {
+
+    static async getUsers(component: ComponentPublicInstance, id: number) {
+        try {
+            const response = await component.axios.get(ConstantTool.BASE_URL + '/user', {
+                headers: { Authorization: useAppStore().session.token, },
+            })
+            const users = JsonTool.jsonConvert.deserializeArray(response.data, User)
+            return Promise.resolve({result: users})
+        } catch(e) {
+            return Promise.reject(e)
+        }
+    }
+
+    static async getUser(component: ComponentPublicInstance) {
+        try {
+            const response = await component.axios.get(ConstantTool.BASE_URL + '/user', {
+                headers: { Authorization: useAppStore().session.token, },
+            })
+            const user = JsonTool.jsonConvert.deserializeObject(response.data, User)
+            return Promise.resolve({result: user})
+        } catch(e) {
+            return Promise.reject(e)
+        }
+    }
+
+    static async postUser(component: ComponentPublicInstance, payload: User) {
+        try {
+            const response = await component.axios.post(ConstantTool.BASE_URL + '/user', payload, {
+                headers: { Authorization: useAppStore().session.token, },
+            })
+            const user = JsonTool.jsonConvert.deserializeObject(response.data, User)
+            return Promise.resolve({ result: user })
+        } catch(e) {
+            return Promise.reject(e)
+        }
+    }
+
+    static async deleteUser(component: ComponentPublicInstance, id: number) {
+        try {
+            const response = await component.axios.delete(ConstantTool.BASE_URL + '/user/' + id, {
+                headers: { Authorization: useAppStore().session.token, },
+            })
+            const user = JsonTool.jsonConvert.deserializeObject(response.data, User)
+            return Promise.resolve({ result: user })
+        } catch(e) {
+            return Promise.reject(e)
+        }
+    }
+
+}
