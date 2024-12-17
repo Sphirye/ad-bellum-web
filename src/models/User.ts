22998567cbf70845cdd9824399b84b8efb2e0083
Roniel Contreras <ronielcontreras70@gmail.com>
2024-12-17 12:57:22 -0500

    Add username and password fields to user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,9 +7,16 @@ export default class User extends Auditing {
     @JsonProperty("id", Number)
     id?: number = undefined
 
+    @JsonProperty("username", String, true)
+    username?: string = undefined
+
     @JsonProperty("email", String)
     email?: string = undefined
 
     @JsonProperty("authorities", [Authority], true)
     authorities?: Authority[] = undefined
+
+    @JsonProperty("password", String, true)
+    password?: string = undefined
+
 }
