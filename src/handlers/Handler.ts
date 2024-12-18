fd0c32c9f404292ece1659969f0d581e17e756d4
Roniel Contreras <ronielcontreras70@gmail.com>
2024-12-18 13:07:44 -0500

    Refactor Handler.ts to allow auxiliar request

--- src/handlers/Handler.ts
+++ src/handlers/Handler.ts
@@ -5,37 +5,61 @@ import Response from "@/models/responses/Response"
 
 export default class Handler {
 
-    static async getItem<T>(component: LoadingUI, content: SingleItem<T>, service: () => Promise<Response<T>>) {
-        try {
-            if (!("item" in content)) { throw new LogDisplayError(LogDisplayError.PROPERTY_DOES_NOT_EXISTS_IN_CONTENT) }
-            component.loading = true
-            if ("progress" in component) { component.progress = true }
-            const response = await service()
-            content.item = response.result
-        } catch (e) {
-            console.log(e)
-        } finally {
-            component.loading = false
-            if ("progress" in component) { component.progress = false }
+    static async sampleRequest<T>(state: LoadingUI, service: () => Promise<Response<T>>) {
+        return await this._request(state, service)
+    }
+
+    static async getItem<T>(
+        state: LoadingUI,
+        content: SingleItem<T>,
+        service: () => Promise<Response<T>>,
+    ) {
+        if (!("item" in content)) { throw new LogDisplayError(LogDisplayError.PROPERTY_DOES_NOT_EXISTS_IN_CONTENT) }
+
+        const response =  await this._request(state, service) as Response<T>
+        content.item = response.result
+    }
+
+    static async getItems<T>(
+        state: LoadingUI,
+        content: MultipleItem<T>,
+        service: () => Promise<Response<T[]>>,
+    ) {
+        if (!("items" in content)) {
+            throw new LogDisplayError(LogDisplayError.PROPERTY_DOES_NOT_EXISTS_IN_CONTENT)
+        }
+
+        const response = await this._request(state, service) as Response<T[]>
+        content.items.splice(0, content.items.length)
+        content.items.push(...response?.result)
+
+        if ("totalItems" in content) {
+            content.totalItems = response.xTotalCount
         }
     }
 
-    static async getItems<T>(component: LoadingUI, content: MultipleItem<T>, service: () => Promise<Response<T[]>>) {
+    static async _request<T>(
+        state: LoadingUI,
+        service: () => Promise<Response<T | T[]>>
+    ) {
         try {
-            if (!("items" in content)) { throw new LogDisplayError(LogDisplayError.PROPERTY_DOES_NOT_EXISTS_IN_CONTENT) }
-            component.loading = true
-            if ("progress" in component) { component.progress = true }
-            const response = await service()
-            content.items.splice(0, content.items.length)
-            content.items.push(...response.result)
-            if ("totalItems" in content) { content.totalItems = response.xTotalCount }
+            this._prepareState(state)
+            return await service()
+        } catch(e) {
+            console.log(e)
+        } finally {
+            this._finishState(state)
         }
-        catch (e)
-        { console.log(e) }
-        finally {
-            component.loading = false
-            if ("progress" in component) { component.progress = false }
-        }
+    }
+
+    private static async _prepareState<T>(state: LoadingUI) {
+        state.loading = true
+        if ("progress" in state) { state.progress = true }
+    }
+
+    private static _finishState<T>(state: LoadingUI) {
+        state.loading = false
+        if ("progress" in state) { state.progress = false }
     }
 
 }
