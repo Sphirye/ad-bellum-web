715ba0796e1d9f5c630fd079e81a88e04704bfbd
Roniel Contreras <ronielcontreras70@gmail.com>
2024-12-18 22:24:46 -0500

    Add ScoreProfile model

---
+++ src/models/ScoreProfile.ts
@@ -0,0 +1,24 @@
+import { JsonObject, JsonProperty } from "json2typescript";
+import Auditing from "./Auditing";
+
+@JsonObject("ScoreProfile")
+export default class ScoreProfile extends Auditing {
+
+    @JsonProperty("id", Number, true)
+    id?: number = undefined
+
+    @JsonProperty("name", String, true)
+    name?: string = undefined
+
+    @JsonProperty("cuts", Number, true)
+    cuts?: number = undefined
+
+    @JsonProperty("slices", Number, true)
+    slices?: number = undefined
+
+    @JsonProperty("controls", Number, true)
+    controls?: number = undefined
+
+    @JsonProperty("timeLimitInSeconds", Number, true)
+    timeLimitInSeconds?: number = undefined
+}
