4198307dcb76fb0121004fbc6707fed21d192e93
Roniel Contreras <ronielcontreras70@gmail.com>
2025-01-02 19:13:23 -0500

    Encapsulate fight creation view in a form

--- src/pages/fights/create.vue
+++ src/pages/fights/create.vue
@@ -1,85 +1,87 @@
 <template>
   <v-container fluid background-color="white">
-    <v-row dense justify="center" align="center">
-
-      <v-col cols="12">
-        <v-sheet color="grey-darken-1" class="py-3 px-3 my-2">
-          <v-btn variant="text" icon @click="router.back()" class="mr-2" density="compact">
-            <v-icon icon="mdi-arrow-left"/>
-          </v-btn>
-          Crear combate
-        </v-sheet>
-      </v-col>
-
-      <v-col cols="6">
-        <v-select
-          v-model="match.fencer_1_id"
-          :items="fencers.items"
-          item-title="name"
-          item-value="id"
-          rounded="0"
-          label="Esgrimista 1"
-          variant="outlined"
-          density="comfortable"
-          hide-details
-          dense
-        />
-      </v-col>
-
-      <v-col cols="6">
-        <v-select
-          v-model="match.fencer_2_id"
-          :items="fencers.items"
-          item-title="name"
-          item-value="id"
-          rounded="0"
-          label="Esgrimista 2"
-          variant="outlined"
-          density="comfortable"
-          hide-details
-          dense
-        />
-      </v-col>
-
-      <v-col cols="12">
-        <v-autocomplete
-          v-model="match.scoreProfile"
-          :items="scoreProfiles.items"
-          return-object
-          item-title="name"
-          rounded="0"
-          label="Perfil de Puntos"
-          variant="outlined"
-          density="comfortable"
-          hide-details
-          dense
-        />
-      </v-col>
-    </v-row>
-
-    <ScoreProfileDetails
-      :model="match.scoreProfile"
-      :editable="true"
-      hide-name
-    />
-
-    <v-divider
-      opacity="50%"
-      class="my-3"
-    />
-
-    <v-row dense>
-
-      <v-spacer/>
-
-      <v-btn
-        color="primary"
-        variant="flat"
-        @click="createMatch()"
-      >
-        Crear
-      </v-btn>
-    </v-row>
+    <v-form ref="form">
+      <v-row dense justify="center" align="center">
+  
+        <v-col cols="12">
+          <v-sheet color="grey-darken-1" class="py-3 px-3 my-2">
+            <v-btn variant="text" icon @click="router.back()" class="mr-2" density="compact">
+              <v-icon icon="mdi-arrow-left"/>
+            </v-btn>
+            Crear combate
+          </v-sheet>
+        </v-col>
+  
+        <v-col cols="6">
+          <v-select
+            v-model="match.fencer_1_id"
+            :items="fencers.items"
+            item-title="name"
+            item-value="id"
+            rounded="0"
+            label="Esgrimista 1"
+            variant="outlined"
+            density="comfortable"
+            hide-details
+            dense
+          />
+        </v-col>
+  
+        <v-col cols="6">
+          <v-select
+            v-model="match.fencer_2_id"
+            :items="fencers.items"
+            item-title="name"
+            item-value="id"
+            rounded="0"
+            label="Esgrimista 2"
+            variant="outlined"
+            density="comfortable"
+            hide-details
+            dense
+          />
+        </v-col>
+  
+        <v-col cols="12">
+          <v-autocomplete
+            v-model="match.scoreProfile"
+            :items="scoreProfiles.items"
+            return-object
+            item-title="name"
+            rounded="0"
+            label="Perfil de Puntos"
+            variant="outlined"
+            density="comfortable"
+            hide-details
+            dense
+          />
+        </v-col>
+      </v-row>
+  
+      <ScoreProfileDetails
+        :model="match.scoreProfile"
+        :editable="true"
+        hide-name
+      />
+  
+      <v-divider
+        opacity="50%"
+        class="my-3"
+      />
+  
+      <v-row dense>
+  
+        <v-spacer/>
+  
+        <v-btn
+          color="primary"
+          variant="flat"
+          @click="createMatch()"
+        >
+          Crear
+        </v-btn>
+      </v-row>
+    </v-form>
   </v-container>
 </template>
 
@@ -136,15 +138,22 @@ export default defineComponent({
     },
 
     async createMatch() {
-      let response: SingleItem<Match> = { item: new Match() }
-
-      await Handler.getItem(this, response, () =>
-        MatchService.postMatch(this, this.match)
-      )
-
-      if (response.item.id) {
-        this.router.push("/fights/" + response.item.id + "/manage")
+      if (await this.isFormValid()) {
+        let response: SingleItem<Match> = { item: new Match() }
+  
+        await Handler.getItem(this, response, () =>
+          MatchService.postMatch(this, this.match)
+        )
+  
+        if (response.item.id) {
+          this.router.push("/fights/" + response.item.id + "/manage")
+        }
       }
+    },
+
+    async isFormValid() {
+      const { valid } = await (this.$refs['form'] as any).validate()
+      return valid as boolean
     },
   }
 })
